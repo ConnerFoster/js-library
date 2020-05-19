c62df7a4f8725eb1175a7a8953f25bfb23467828
Conner Foster <connerf99@gmail.com>
2020-05-18 23:05:16 -0400

    saving

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-let modal = document.getElementById('form-modal');
-let createBookBtn = document.getElementById('create-book-btn');
+let modal = document.getElementById('modal')
+let createBookBtn = document.getElementById('create-book-btn')
+let library = []
+let books = document.getElementById('books')
+
+class Book {
+    constructor(title,author,pageCount) {
+    this.title = title;
+    this.author = author
+    this.pageCount = pageCount
+}
+}
+document.querySelector('#submit-form-btn').addEventListener('submit',addBook)
+function addBook(e) {
+    e.preventDefault()
+    
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('page-count').value
+    let thisBook = new Book(title, author, pages)
+    library.push(thisBook)
+    render(thisBook)
+    
+
+}
+
+function render(book){
+    books.innerHTML += `
+    <div class="book-tile justify-content-center d-inline-flex text-capitalize container pt-6">
+        <div class="tile-text">
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <p>${book.pageCount}</p>
+        </div>
+        </div>
+
+    `
+}
+
+const hobbit = new Book("hobbit", "tolkien", 300)
+render(hobbit);
+
+
 
 createBookBtn.onclick = () => {
-    modal.style.display = "block";
+    modal.style.display = "block"
 }
+
