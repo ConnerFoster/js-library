3db8932dd05c6b3d3fec15a3841a965f0b8ad83d
Conner Foster <connerf99@gmail.com>
2020-05-18 21:23:11 -0400

    not sure why modal wont open

---
+++ app.js
@@ -0,0 +1,6 @@
+let modal = document.getElementById('form-modal');
+let createBookBtn = document.getElementById('create-book-btn');
+
+createBookBtn.onclick = function() {
+    modal.style.display = "block;";
+}
