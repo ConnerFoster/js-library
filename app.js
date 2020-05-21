17f1e876e6dddb97c944b119c6f0fb99b9572e46
Conner Foster <connerf99@gmail.com>
2020-05-20 20:55:29 -0400

    starting over to restructure code and add local storage

--- app.js
+++ app.js
@@ -1,4 +1,70 @@
-let modal = document.getElementById('modal')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let modal = document.getElementById('modal')
 let createBookBtn = document.getElementById('create-book-btn')
 let library = []
 let books = document.getElementById('books')
@@ -36,19 +102,20 @@ function addBook(e) {
 
 function render(book){
     books.innerHTML += `
-    <div class="book-tile justify-content-center d-inline-flex text-capitalize container pt-6">
+    <div class="book-tile justify-content-center d-inline-flex align-items-center text-capitalize container pt-6">
         <div class="tile-text">
         <p>${book.title}</p>
         <p>${book.author}</p>
         <p>${book.pageCount}</p>
+        <div class="delete-button-div>
+        <a href="#" id="delete-btn" class="btn btn-danger btn-sm delete-btn" delete>x</a>
         </div>
         </div>
 
     `
 }
 
-const hobbit = new Book("hobbit", "tolkien", 300)
-render(hobbit);
+
 
 
 
@@ -64,3 +131,19 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
   }
+
+  const hobbit = new Book("hobbit", "tolkien", 300)
+  render(hobbit)
+
+function deleteBook(e){
+    e.parentElement.parentElement.remove();
+}
+
+document.getElementById('delete-btn').addEventListener('click',deleteBook)
+
+deleteButtons = document.getElementsByClassName('delete-btn')
+
+deleteButtons.onclick = () => {
+    //deleteButtons.parentElement.remove()
+    deleteButtons.parentElement.parentElement.remove()
+}*/
