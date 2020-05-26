c127b7a1e5dd870ae8dd5cd41c5e248fd3d11917
Conner Foster <connerf99@gmail.com>
2020-05-25 22:05:55 -0400

    finished for now

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Book Class: Represents a Book
 class Book {
     constructor(title, author, pageCount) {
         this.title = title;
@@ -86,10 +85,10 @@ class Store {
           localStorage.setItem('books', JSON.stringify(books));
         }
       
-        static removeBook(isbn) {
+        static removeBook(title) {
           const books = Store.getBooks();
       
-          books.forEach((book, title) => {
+          books.forEach((book, index) => {
             if(book.title === title) {
               books.splice(index, 1);
             }
@@ -132,7 +131,7 @@ document.querySelector('#individual-books').addEventListener('click', (e) => {
     Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
     UI.showRemoveAlert('Book Removed','success')
 
-    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+    Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
 })
 
 
@@ -155,149 +154,7 @@ window.onclick = function(event) {
     }
   }
 
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let modal = document.getElementById('modal')
-let createBookBtn = document.getElementById('create-book-btn')
-let library = []
-let books = document.getElementById('books')
-let submitBtn = document.getElementById('submit-form-btn')
-
-class Book {
-    constructor(title,author,pageCount) {
-    this.title = title;
-    this.author = author
-    this.pageCount = pageCount
-}
-}
-document.querySelector('#submit-form-btn').addEventListener('submit',addBook)
-function addBook(e) {
-    e.preventDefault()
-    
-    let title = document.getElementById('title').value
-    let author = document.getElementById('author').value
-    let pages = document.getElementById('page-count').value
-    if(title === "" || author === "" || pages==="") {
-        alert("all fields required.")
-    }
-    else {
-        let thisBook = new Book(title, author, pages)
-        library.push(thisBook)
-        render(thisBook)
-        modal.style.display = "none;"
-        document.getElementById("myForm").reset();
-    }
-    
-    
-    
-
-}
-
-function render(book){
-    books.innerHTML += `
-    <div class="book-tile justify-content-center d-inline-flex align-items-center text-capitalize container pt-6">
-        <div class="tile-text">
-        <p>${book.title}</p>
-        <p>${book.author}</p>
-        <p>${book.pageCount}</p>
-        <div class="delete-button-div>
-        <a href="#" id="delete-btn" class="btn btn-danger btn-sm delete-btn" delete>x</a>
-        </div>
-        </div>
-
-    `
-}
-
-
-
-
-
-createBookBtn.onclick = () => {
-    modal.style.display = "block"
-}
-
-
-submitBtn.addEventListener('click',addBook)
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
-  const hobbit = new Book("hobbit", "tolkien", 300)
-  render(hobbit)
-
-function deleteBook(e){
-    e.parentElement.parentElement.remove();
-}
-
-document.getElementById('delete-btn').addEventListener('click',deleteBook)
-
-deleteButtons = document.getElementsByClassName('delete-btn')
-
-deleteButtons.onclick = () => {
-    //deleteButtons.parentElement.remove()
-    deleteButtons.parentElement.parentElement.remove()
-}*/
+  
+  
